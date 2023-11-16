56c2d5f64dddfffadd424508d231190aebeacf2f
DURKA-SANTHI <124854878+DURKA-SANTHI@users.noreply.github.com>
2023-11-16 12:16:17 +0530

    Day 1

---
+++ bankapp/src/App.js
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Helloworld1 from "./components/Helloworld1";
+import Helloworld from "./components/Helloworld";
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld/>
+      <Helloworld1/>
+      <Greeting/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bankapp/src/components/Greeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Greeting(){
+    return (
+      <div>
+          <h1>Greeting Humans!!!!!!!!!</h1>
+      </div>
+    ); 
+  }
+  
+  export default Greeting;
